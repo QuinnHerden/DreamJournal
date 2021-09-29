1f0a9eeae3a7eaef8994ded12c5c8c7738ed8fb6
Quinn Herden <herdenq@gmail.com>
2021-09-29 19:44:11 -0400

    add modal log in

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,5 +1,19 @@
+// Dropdown
 $(".navbar-burger").click(function() {
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
 })
 
+
+// Modal Care
+$("#launch_modal").click(function() {
+    $(".modal").toggleClass("is-active");
+})
+
+$(".modal-close").click(function() {
+    $(".modal").toggleClass("is-active");
+})
+
+$(".modal-background").click(function() {
+    $(".modal").toggleClass("is-active");
+})
