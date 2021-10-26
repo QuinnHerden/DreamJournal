65278ac909d77a557a47cc74f2bc223d01a45d76
Quinn Herden <herdenq@gmail.com>
2021-10-26 14:59:55 -0400

    connect client and server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const path = require('path')
+require('dotenv').config()
 
+// const usersController = require('./controllers/users')
+// const postsController = require('./controllers/posts')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3100
 
-// app
-//   .use('/', express.static(path.join(__dirname, '../docs'))) //current path + '../docs'
+app
+  .use('/', express.static(path.join(__dirname, '../docs')))
+  // .use('/users', usersController)
+  // .use('/posts', postsController)
 
 app
   .get('*', (req, res) => res.sendFile(path.join(__dirname, '../docs/index.html')))
 
 app.listen(port, () => {
-  console.log(`Journal listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
