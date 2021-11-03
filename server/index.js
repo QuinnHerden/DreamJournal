f4a55bdbbe77570c8679e88f02f12aa09b4924d0
Quinn Herden <herdenq@gmail.com>
2021-11-03 14:35:55 -0400

    build project

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const path = require('path')
 require('dotenv').config()
 
-// const usersController = require('./controllers/users')
+const usersController = require('./controllers/users')
 // const postsController = require('./controllers/posts')
 const app = express()
 const port = process.env.PORT || 3100
 
 app
   .use('/', express.static(path.join(__dirname, '../docs')))
-  // .use('/users', usersController)
+  .use('/users', usersController)
   // .use('/posts', postsController)
 
 app
