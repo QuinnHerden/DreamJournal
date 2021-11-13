8787c239c7538e8e9b4cf926c2ef74a1a3035ed0
Quinn Herden <herdenq@gmail.com>
2021-11-13 15:38:48 -0500

    add oruga login warning

---
+++ client/src/components/Messages.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="mesages">
+        <div class="notification is-light" :class="`is-${message.type}`" v-for="(message, i) in messages" :key="i">
+            <button class="delete" @click.prevent="done(i)"></button>
+            {{message.text + "hi"}}
+        </div>
+    </div>
+</template>
+<script>
+import session from "../services/session";
+export default {
+    data: ()=> ({
+        messages: session.messages
+    }),
+    methods: {
+        done(i){
+            this.messages.splice(i, 1);
+            
+        }
+    }
+}
