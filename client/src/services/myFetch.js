adb78cda2ddde5997e72b78e6d1d2c93d87cc5d2
Quinn Herden <herdenq@gmail.com>
2021-11-17 15:04:59 -0500

    built

--- client/src/services/myFetch.js
+++ client/src/services/myFetch.js
@@ -1,6 +1,6 @@
 import session from "./session";
 
-const API_ROOT = process.env.VUE_APP_API_ROOT ?? 'http://localhost:3000/';
+const API_ROOT = process.env.VUE_APP_API_ROOT;
 
 export async function api(url, data = null, method = null){
     try {
