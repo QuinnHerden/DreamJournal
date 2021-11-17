885ca12a022074d409454d6d03392ade32b1c519
Quinn Herden <herdenq@gmail.com>
2021-11-17 09:07:47 -0500

    environment var for api root

--- client/src/services/myFetch.js
+++ client/src/services/myFetch.js
@@ -1,5 +1,4 @@
-// const API_ROOT = `http://localhost:${process.env.PORT}/`;
-const API_ROOT = `http://localhost:3000/`;
+const API_ROOT = process.env.VUE_APP_API_ROOT ?? 'http://localhost:3100/'
 
 export function api(url) {
     return fetch(API_ROOT + url).then(x => x.json())
